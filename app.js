db12428f42fe847e7957a6ae4fc9e39fd4902396
Gal Rettig <galrettig@gmail.com>
2017-02-11 20:36:31 +0200

    app.js updated with comments

--- app.js
+++ app.js
@@ -3,9 +3,14 @@
  */
 /**
  * Created by galrettig on 2/9/17.
+ * MIT license.
  */
 
 
+/**
+ * @description - main object, holds imported dependencies and opts
+ * @type {{yt: *, yts: *, fs, opts: {maxResults: number, key: string}}}
+ */
 var ytdns = {
     yt : require('ytdl-core'),
     yts : require('youtube-search'),
@@ -13,11 +18,18 @@ var ytdns = {
     opts : {maxResults: 5, key: "YOUR-YOUTUBE-API-KEY-HERE"}
 };
 
+/**
+ *
+ * @param args - usage args -s[search mode accepts a key-word] -d[download mode accepts a url to download] -sd[search and download mode accepts a keyword]
+ * @returns {null}
+ */
+
 ytdns.handle_args = function(args){
     var argument = args[0];
     if(argument == null) {
         return null;
     } else {
+        // control flow on which mode will be used
         switch (argument) {
             case "-s" :
                 this.search(args.slice(1), null);
@@ -35,6 +47,12 @@ ytdns.handle_args = function(args){
 
     }
 };
+
+/**
+ *
+ * @param args - rest of the arguments, key-word to search in youtube
+ * @param cb - a callback function to be invoked when the list of results returns..
+ */
 ytdns.search = function (args,cb) {
     console.log(args);
     var key_word = args[0];
@@ -47,32 +65,42 @@ ytdns.search = function (args,cb) {
         else if(cb != null) {
             cb(results);
         } else {
-
+            //TODO: if no callback provided call a default one,
         }
     });
 
 };
 //-d url file_name
+/**
+ * @description - download handler
+ * @param args - youtube url to download
+ */
 ytdns.download = function (args) {
-    console.log("download-args : " + args + "\ndownload-args-END");
     var url = args[0];
     var video_name = args[1];
     this.yt(url).pipe(this.fs.createWriteStream("./downloads/" + video_name));
 
 };
 //-sd key_word
+/**
+ * @description - search and download mode
+ * @param args - keyword to search and download
+ */
 ytdns.search_and_download = function (args) {
     var that = this;
-    console.log("search_and_Download");
-    var index = args[1];
+    var index = args[1];// index to download instead of default item which is 0
+
+    // callback to pass later to this.search function
     function cb (res){
+        //search found something
         if(res.length > 0){
+            // received option to download a different item then 0 in the list
             if(index != null) {
                 console.log(res[i]);
-                that.download([res[i].link, (res[i].title).replace(/ /gi,"_") + ".mp4"]);
+                that.download([res[i].link, that.handle_file_name(res[i].title) + ".mp4"]);
             } else {
                 console.log(res[0]);
-                that.download([res[0].link, (res[0].title).replace(/ /gi,"_") + ".mp4"]);
+                that.download([res[0].link, that.handle_file_name(res[0].title) + ".mp4"]);
             }
         }
     };
